ce14be278ed4539d11fb741af51e4cfd9e15edcd
Ayaan2907 <ayaanansari7921@outlook.com>
2020-10-19 10:00:32 +0530

    included all constants

--- constants.h
+++ constants.h
@@ -2,7 +2,19 @@
 
 #define constants
 
-// declaration of game states
 const int BEFORE_START = 0,
-          AFTER_START  = 1;
-          
+          STARTED = 1;
+
+const int UP = 259,
+          DOWN = 258,
+          LEFT = 260,
+          RIGHT = 261;
+
+const int FOOD_COLOR_PAIR = 1;
+const int SNAKE_COLOR_PAIR = 7;
+const int BORDER_COLOR_PAIR = 1;
+const int STATUS_COLOR_PAIR = 5;
+
+const int FOOD_COUNT = 10;
+
+#endif
